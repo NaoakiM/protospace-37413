5ae07ca34325f34a2ba0622adfc971bf11d885e8
NaoakiM <97227459+NaoakiM@users.noreply.github.com>
2022-02-03 21:27:20 +0900

    テーブル修正

--- db/migrate/20220201105544_create_comments.rb
+++ db/migrate/20220201105544_create_comments.rb
@@ -1,8 +1,8 @@
 class CreateComments < ActiveRecord::Migration[6.0]
   def change
     create_table :comments do |t|
-      t.references :user_id, foreign_key: true
-      t.references :prototype_id
+      t.references :user, foreign_key: true
+      t.references :prototype, foreign_key: true
       t.text :text
       t.timestamps
     end

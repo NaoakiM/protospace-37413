e07b975efddb4002b53d213120514b4732033d9c
NaoakiM <97227459+NaoakiM@users.noreply.github.com>
2022-02-03 21:11:29 +0900

    references型に変更

--- db/migrate/20220201105544_create_comments.rb
+++ db/migrate/20220201105544_create_comments.rb
@@ -1,8 +1,8 @@
 class CreateComments < ActiveRecord::Migration[6.0]
   def change
     create_table :comments do |t|
-      t.integer :user_id, foreign_key: true
-      t.integer :prototype_id
+      t.references :user_id, foreign_key: true
+      t.references :prototype_id
       t.text :text
       t.timestamps
     end

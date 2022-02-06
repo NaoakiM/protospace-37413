b42f1ca92a3f5fe70fa7e8e0976466d5cbe1a5f5
NaoakiM <97227459+NaoakiM@users.noreply.github.com>
2022-02-06 20:24:50 +0900

    before_action修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,7 +1,7 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!,only: [:edit, :new, :destroy]
   before_action :set_prototype,only: [:edit, :show, :update]
-  before_action :move_to_index, except: [:index, :show]
+  before_action :move_to_index, except: [:index, :show, :new]
   def index
     @prototypes = Prototype.all
   end
